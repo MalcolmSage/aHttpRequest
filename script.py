c97750c4731a162336bc47c1a389f3cd1f722d61
llamar <thatlivelyllama@gmail.com>
2022-03-20 11:36:00 +0000

    Added new request handling and separate packaging

--- script.py
+++ script.py
@@ -2,12 +2,44 @@ import sys, json, requests
 
 
 
+def outputPackaging(response):
+    prepackaged = {
+        "Status-code": response.status_code,
+    }
+    packaged = json.dumps(prepackaged)
+    # print(packaged)
+    return packaged
+
+
+def request_handling(request):
+    response = requests.get(request)
+    response.close()
+    return outputPackaging(response)
+
+def data_handling(data):
+# Take in the data as list
+    print("Please wait")
+    url_list = data.split('\n')[:-1]
+    outputFile = "stdout.txt"
+    newList = open(outputFile, "w")
+    
+    # iterate through list
+    for url in url_list:
+        # handle request in request_handling
+        newList.write(request_handling(url))
+        
+    print("Done")
+    return url_list
 
 def data_input():
 # Read input from stdin in newline format
     print("Enter the urls:\n")   
-    userInput = sys.stdin.readlines()
-    return userInput
+    user_input = ""
+    for line in sys.stdin:
+        if line == '\n':
+            break
+        user_input += line
+    return data_handling(user_input)
 
 
 if __name__ == "__main__":
--- test_script.py
+++ test_script.py
@@ -2,15 +2,23 @@ import unittest
 import sys
 from unittest.mock import patch
 from io import StringIO
-from script import data_input
+from script import data_input, data_handling
 
 class functionTesting(unittest.TestCase):
-    @patch('sys.stdin', StringIO('http://www.bbc.co.uk/iplayer'))
+    @patch('sys.stdin', StringIO("http://www.bbc.co.uk/iplayer\nhttp://www.bbc.co.uk/iplayer\n"))
     
     def test_standard_input(self): 
         # Check input and output match
-        expected_out = ['http://www.bbc.co.uk/iplayer']
+        expected_out = ['http://www.bbc.co.uk/iplayer', 'http://www.bbc.co.uk/iplayer']
         self.assertEqual(data_input(), expected_out)
+        
+    def test_list_input(self):
+        newline_list = "http://www.bbc.co.uk/iplayer\nhttp://www.bbc.co.uk/iplayer\n"
+        expected_out = ['http://www.bbc.co.uk/iplayer', 'http://www.bbc.co.uk/iplayer']
+        self.assertEqual(data_handling(newline_list), expected_out)
+        
+    
+        
         
     
 
